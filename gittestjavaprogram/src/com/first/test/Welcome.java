ad3ff9a395e8e5c4028384d1e18cc8f873946cc1
rathnam <rathnam@sattvaq.com>
2015-10-08 19:19:29 +0530

    i added hai statement

--- gittestjavaprogram/src/com/first/test/Welcome.java
+++ gittestjavaprogram/src/com/first/test/Welcome.java
@@ -7,6 +7,8 @@ public class Welcome
 	System.out.println("welcome to github");
 	System.out.println("second time i commited");
 	System.out.println("third time i added statement");
-
+	System.out.println("fourth statem i added");
+	System.out.println("fifth time i added the statement");
+System.out.println("testing github");
 }
 }
